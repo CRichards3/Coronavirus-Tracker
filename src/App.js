770a8be590986b5ec80393767a1b740bcd675d73
Connor <ctricha1@asu.edu>
2020-04-21 17:18:06 -0700

    added chart, country dropdown, cleaned up cards

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import React from "react";
 import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
+import { fetchData } from "./api";
 
 import styles from "./App.module.css";
 
 class App extends React.Component {
+  state = {
+    data: {},
+    country: ""
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async country => {
+    //fetch data, then set state
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
-        <CountryPicker />
-        <Chart />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
